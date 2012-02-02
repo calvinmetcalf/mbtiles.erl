a50c95e124d10da94a4071333b7b8befd3983dd2
Calvin Metcalf <calvin.metcalf@state.ma.us>
2012-02-02 15:43:13 -0500

    can now create mbtiles

--- src/mbtiles.erl
+++ src/mbtiles.erl
@@ -10,6 +10,7 @@
 start(Arg) -> gen_server:start_link({local, Arg}, ?MODULE, Arg, []).
 stop(Which) -> gen_server:call(Which, stop).
 
+
 %%@spec get(What::{tile|grid},{tms|xyz|[]}, Zoom, X, Y)
 %get a tile or grid
 get(Which, What, tms, Z, X, Y) -> gen_server:call(Which, {get, What, Z, X, Y});
@@ -56,7 +57,7 @@ handle_call(stop, _From, D) ->
 init(M) ->
 case checkMBTILES(M) of
 true -> {ok, element(2,sqlite3:start_link(m,[{file, getTilePath(M)}]))};
-false -> throw(noSuchTileset)
+false -> createMBTILES(M)
 end.
 
 
@@ -110,6 +111,81 @@ lists:reverse(tl(lists:reverse(lists:flatten(lists:map(fun({A,B}) -> [34,A,34,58
 
 pi(I) ->
 lists:append([[103,114,105,100,40],I,[41,59]]).
+
+createMBTILES(M) ->
+D = {ok, element(2,sqlite3:start_link(m,[{file, getTilePath(M)}]))},
+%%MBTiles schema taken from https://github.com/mapbox/node-mbtiles/blob/master/lib/schema.sql
+sqlite3:sql_exec(element(2,D), ["CREATE TABLE IF NOT EXISTS map (
+   zoom_level INTEGER,
+   tile_column INTEGER,
+   tile_row INTEGER,
+   tile_id TEXT,
+   grid_id TEXT
+);
+
+CREATE TABLE IF NOT EXISTS grid_key (
+    grid_id TEXT,
+    key_name TEXT
+);
+
+CREATE TABLE IF NOT EXISTS keymap (
+    key_name TEXT,
+    key_json TEXT
+);
+
+CREATE TABLE IF NOT EXISTS grid_utfgrid (
+    grid_id TEXT,
+    grid_utfgrid BLOB
+);
+
+CREATE TABLE IF NOT EXISTS images (
+    tile_data blob,
+    tile_id text
+);
+
+CREATE TABLE IF NOT EXISTS metadata (
+    name text,
+    value text
+);
+
+
+CREATE UNIQUE INDEX IF NOT EXISTS map_index ON map (zoom_level, tile_column, tile_row);
+CREATE UNIQUE INDEX IF NOT EXISTS grid_key_lookup ON grid_key (grid_id, key_name);
+CREATE UNIQUE INDEX IF NOT EXISTS keymap_lookup ON keymap (key_name);
+CREATE UNIQUE INDEX IF NOT EXISTS grid_utfgrid_lookup ON grid_utfgrid (grid_id);
+CREATE UNIQUE INDEX IF NOT EXISTS images_id ON images (tile_id);
+CREATE UNIQUE INDEX IF NOT EXISTS name ON metadata (name);
+
+
+CREATE VIEW IF NOT EXISTS tiles AS
+    SELECT
+        map.zoom_level AS zoom_level,
+        map.tile_column AS tile_column,
+        map.tile_row AS tile_row,
+        images.tile_data AS tile_data
+    FROM map
+    JOIN images ON images.tile_id = map.tile_id;
+
+CREATE VIEW IF NOT EXISTS grids AS
+    SELECT
+        map.zoom_level AS zoom_level,
+        map.tile_column AS tile_column,
+        map.tile_row AS tile_row,
+        grid_utfgrid.grid_utfgrid AS grid
+    FROM map
+    JOIN grid_utfgrid ON grid_utfgrid.grid_id = map.grid_id;
+
+CREATE VIEW IF NOT EXISTS grid_data AS
+    SELECT
+        map.zoom_level AS zoom_level,
+        map.tile_column AS tile_column,
+        map.tile_row AS tile_row,
+        keymap.key_name AS key_name,
+        keymap.key_json AS key_json
+    FROM map
+    JOIN grid_key ON map.grid_id = grid_key.grid_id
+    JOIN keymap ON grid_key.key_name = keymap.key_name;"]),
+D.
 
 handle_cast(_Msg, State) -> {noreply, State}.
 handle_info(_Info, State) -> {noreply, State}.
