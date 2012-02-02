5d77a977f58069858d42c6694724a5cc4e9341e0
Calvin Metcalf <calvin.metcalf@state.ma.us>
2012-02-02 09:30:56 -0500

    added tile info and some error catching

--- src/mbtiles.erl
+++ src/mbtiles.erl
@@ -1,6 +1,6 @@
 -module(mbtiles).
 -behaviour(gen_server).
--export([start/1, stop/1, get/5, get/6]).
+-export([start/1, stop/1, get/2, get/5, get/6]).
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
@@ -17,6 +17,8 @@ get(Which, What, xyz, Z, X, Y) -> gen_server:call(Which, {get, What, Z, X, flipY
 
 get(Which, What, Z, X, Y) -> get(Which, What, xyz, Z, X, Y).
 
+get(Which, I) -> gen_server:call(Which, {get, I}).
+
 flipY(Y,Z) ->
 try round(math:pow(2,Z) - Y - 1)
 catch
@@ -32,6 +34,18 @@ end,
 {reply, Reply, D};
 handle_call({get,grid,Z,X,Y}, _From, D)->
 Reply = lists:append([fetchGrid(D,Z,X,Y),fetchKey(D,Z,X,Y),[125,125,41,59]]),
+{reply, Reply, D};
+handle_call({get,_,_,_,_}, _From, D)->
+Reply = {noIdea},
+{reply, Reply, D};
+handle_call({get,info.json}, _From, D)->
+Reply = cleanInfo(sqlite3:read_all(D, metadata)),
+{reply, Reply, D};
+handle_call({get,info.jsonp}, _From, D)->
+Reply = pi(cleanInfo(sqlite3:read_all(D, metadata))),
+{reply, Reply, D};
+handle_call({get,_}, _From, D)->
+Reply = {noIdea},
 {reply, Reply, D};
 handle_call(stop, _From, D) ->
 {stop, normal, stopped, D}.
@@ -71,6 +85,12 @@ case sqlite3:sql_exec(D, lists:concat(["select key_name, key_json FROM grid_data
 true -> throw(noSuchKey)
 end.
 
+cleanInfo(D)->
+%with much thanks to http://stackoverflow.com/questions/3923400/erlang-tuple-list-into-json for the following
+Original = lists:map(fun({B,C})->{binary_to_list(B),binary_to_list(C)} end,element(2,hd(tl(D)))),
+StingConverted = [ {X,list_to_binary(Y)} || {X,Y} <- Original ],
+mochijson2:encode(StingConverted).
+
 pg(G) ->
 L = hd(lists:reverse(G)),
 R = tl(lists:reverse(G)),
@@ -82,6 +102,9 @@ lists:reverse(lists:append([[I],R,[40,100,105,114,103]])).
 
 pk(K) ->
 lists:reverse(tl(lists:reverse(lists:flatten(lists:map(fun({A,B}) -> [34,A,34,58,B,44] end, K))))).
+
+pi(I) ->
+lists:append([[103,114,105,100,40],I,[41,59]]).
 
 handle_cast(_Msg, State) -> {noreply, State}.
 handle_info(_Info, State) -> {noreply, State}.
